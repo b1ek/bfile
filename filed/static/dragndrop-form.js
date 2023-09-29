69a695f9fbd95a61fb3d7a0a806c894e926292e3
blek <me@blek.codes>
2023-09-30 01:09:24 +1000

    display some conditional JS text

---
+++ filed/static/dragndrop-form.js
@@ -0,0 +1,11 @@
+(
+    () => {
+        // display the js only text
+        document.getElementById('drag-n-drop-jsonly').style.display = 'initial';
+
+        const drag_rop = document.getElementsByClassName('file-drag-n-drop-inside')[0];
+        const dr_rop_t = document.getElementsByClassName('file-drag-n-drop-inside-text')[0];
+
+        
+    }
+)()

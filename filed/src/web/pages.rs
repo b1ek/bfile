261e57143a79de1216a6615204f1eae8e6572fd2
blek <me@blek.codes>
2023-09-29 23:25:20 +1000

    basic template

--- filed/src/web/pages.rs
+++ filed/src/web/pages.rs
@@ -3,13 +3,20 @@
 */
 
 use warp::{reply::{Reply, Html}, Filter, reject::Rejection};
+use askama::Template;
+
+#[derive(Template)]
+#[template( path = "index.html" )]
+struct Index {
+
+}
 
 pub fn index() -> Html<String> {
-    warp::reply::html("<b>hiiii ^^</b>".into())
+    let rendered = Index {};
+    warp::reply::html(rendered.render().unwrap())
 }
 
 pub fn get_routes() -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-
     let index_r = warp::path::end().map(index);
 
     warp::any().and(index_r)

6bd71711734cbbd5d7d67f6dfc008426bdbc80b6
blek <me@blek.codes>
2023-10-01 20:47:38 +1000

    make the js only script actually work

--- filed/static/js-only.js
+++ filed/static/js-only.js
@@ -1,2 +1,2 @@
-// This script displays all elements with `js-only` class.
-(()=>{let a=document.getElementsByClassName('js-only');for(let i=0;i!=a.length;i++)a[i].style.display='initial'})
+// This script displays all elements with `js-only` class
+(()=>{let a=document.getElementsByClassName('js-only');for(let i=0;i!=a.length;i++)a[i].style.display='initial'})()

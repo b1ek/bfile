b1292a2f4742b54a3a97cea63b2b51d5ab9c5162
blek <me@blek.codes>
2023-10-14 13:19:09 +1000

    minify js

--- containers/rust-dev.Dockerfile
+++ containers/rust-dev.Dockerfile
@@ -5,4 +5,8 @@ RUN cargo install cargo-watch && \
     touch /opt/code/dev-entry.sh && \
     chmod +x /opt/code/dev-entry.sh
 
+RUN apt update && \
+    apt install nodejs npm -y --no-install-recommends && \
+    npm i -g uglify-js
+
 CMD [ "/opt/code/dev-entry.sh" ]
--- filed/build.rs
+++ filed/build.rs
@@ -1,28 +1,59 @@
-use std::{fs, path::PathBuf};
+
+use std::{fs, path::PathBuf, ffi::OsStr, process::Command};
+
 use css_minify::optimizations::{Minifier, Level};
+
+fn asset_path(asset: &PathBuf) -> PathBuf {
+    let mut path = asset.components().take(1).collect::<PathBuf>();
+    path.push(asset.components().last().unwrap());
+    path
+}
+
+fn extfilter(valid: String, x: Option<&OsStr>) -> bool {
+    if x.is_none() {
+        return false
+    }
+    let ext = x.clone().unwrap().to_str().unwrap().to_string();
+    ext == valid
+}
 
 fn main() {
     let assets = fs::read_dir("static/assets").unwrap();
     let assets = assets
         .map(|x| x.unwrap().path())
         .filter(|x| x.extension().is_some())
-        .filter(|x| {
-            let ext = x.extension().clone().unwrap().to_str().unwrap().to_string();
-            ext == "css"
-        })
         .collect::<Vec<PathBuf>>();
     
-    assets.iter().for_each(|asset| {
+    let styles = assets
+        .iter()
+        .filter(|x| {
+            extfilter("css".into(), x.extension())
+        })
+        .collect::<Vec<&PathBuf>>();
+
+    let scripts = assets
+        .iter()
+        .filter(|x| {
+            extfilter("js".into(), x.extension())
+        })
+        .collect::<Vec<&PathBuf>>();
+
+    styles.iter().for_each(|asset| {
         let bundled = Minifier::default().minify(
             String::from_utf8(fs::read(asset).unwrap()).unwrap().as_str(),
             Level::Zero
         ).unwrap();
         
-        fs::write({
-            let mut path = asset.components().take(1).collect::<PathBuf>();
-            path.push(asset.components().last().unwrap());
-            println!("{:?}", path);
-            path
-        }, bundled).unwrap();
+        fs::write(asset_path(asset), bundled).unwrap();
+    });
+
+    scripts.iter().for_each(|asset| {
+        Command::new("uglifyjs")
+            .arg("-c")
+            .arg(asset)
+            .arg("-o")
+            .arg(asset_path(asset))
+            .spawn()
+            .unwrap();
     })
 }

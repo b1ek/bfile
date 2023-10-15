06a281aa0898c8f55c8973516712c6bc76488eb6
blek <me@blek.codes>
2023-10-15 17:38:20 +1000

    trade up unnecessary security for speed

--- filed/src/security.rs
+++ filed/src/security.rs
@@ -1,5 +1,5 @@
 use argon2::{Argon2, Params};
 
 pub fn get_argon2() -> Argon2<'static> {
-    argon2::Argon2::new(argon2::Algorithm::Argon2id, argon2::Version::V0x13, Params::new(65535, 4, 4, Some(64)).unwrap())
+    argon2::Argon2::new(argon2::Algorithm::Argon2id, argon2::Version::V0x13, Params::new(256, 2, 2, Some(32)).unwrap())
 }

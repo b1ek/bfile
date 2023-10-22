2af8e44dd915cf97e03bbf23edbbe6e8d3defddc
blek <me@blek.codes>
2023-10-22 15:16:47 +1000

    change path to comply with swagger spec

--- filed/src/web/api/files/get_all.rs
+++ filed/src/web/api/files/get_all.rs
@@ -29,7 +29,7 @@ pub async fn get_all(state: SharedState) -> Result<Box<dyn Reply>, Rejection> {
 }
 
 pub fn get_all_f(state: SharedState) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    warp::path!("api" / "get_all")
+    warp::path!("api" / "files" / "get_all")
         .map(move || state.clone())
         .and_then(get_all)
 }

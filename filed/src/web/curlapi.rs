82acf6cb50e4183bc647f190ed1b3c79f75e7264
blek <me@blek.codes>
2023-11-02 20:00:39 +1000

    curl API help

--- filed/src/web/curlapi.rs
+++ filed/src/web/curlapi.rs
@@ -3,7 +3,9 @@ use warp::{Filter, reply::Reply, reject::Rejection};
 use super::state::SharedState;
 
 mod upload;
+mod help;
 
 pub fn get_routes(state: SharedState) -> impl Filter<Extract = impl Reply, Error = Rejection> + Clone {
-    upload::get_routes(state)
+    upload::get_routes(state.clone())
+        .or(help::get_routes(state))
 }
